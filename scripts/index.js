2c6282bab9c4be72f9c88d1f67da6b606f36703d
david robison <david robison>
2017-05-27 20:18:33 -0700

    finally index.js shows "undefined"

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
     "http://api.openweathermap.org/data/2.5/weather?zip=98512,us&appid=9275623e0a26c09af9ac2d9854ee8568";
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-      var city = position.name;
-      var state = position.sys.country;
+      var city = position.city;
+      var country = position.country;
       $(".city").html("City: " + city);
-      $(".state").html("State: " + state);
+      $(".country").html("Country: " + country);
     });
   }
 });
