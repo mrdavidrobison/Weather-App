5782dbf760dde17c90fcab080ed65ea5be8ef1ae
david robison <david robison>
2017-05-27 20:13:33 -0700

    update api

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
   var api =
-    "api.openweathermap.org/data/2.5/weather?q=London";
+    "http://api.openweathermap.org/data/2.5/weather?zip=98512,us&appid=9275623e0a26c09af9ac2d9854ee8568";
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       var city = position.name;
       var state = position.sys.country;
-      $("#city").html("City: " + city);
-      $("#state").html("State: " + state);
+      $(".city").html("City: " + city);
+      $(".state").html("State: " + state);
     });
   }
 });
