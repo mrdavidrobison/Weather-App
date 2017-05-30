fceed33b488751e1a1ca0769e2de95726fe5a9ff
david robison <mrdavidrobison@gmail.com>
2017-05-30 12:34:07 -0600

    weather api works

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,49 @@
 $(document).ready(function() {
-  var api =
-    "http://api.openweathermap.org/data/2.5/weather?zip=98512,us&appid=9275623e0a26c09af9ac2d9854ee8568";
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var city = position.city;
-      var country = position.country;
-      $(".city").html("City: " + city);
-      $(".country").html("Country: " + country);
-    });
+
+  var long;
+  var lat;
+  var temp;
+
+  if (navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(function(position){
+      
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      
+      var api = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+long+"&appid=9275623e0a26c09af9ac2d9854ee8568";
+  
+      $.getJSON(api, function(data){
+
+        // JSON calls for Open Weather API
+        var weatherIcon = data.weather.description;
+        var city = data.name;
+        var country = data.sys.country; 
+        var description = data.weather.description;
+        var temp = Math.floor((data.main.temp)*(9/5)-459.67) + " &degF";
+        var humidity = data.main.humidity;
+        var visibility = data.visibility;
+        var wind = data.wind.speed;
+        var sunrise = data.sys.sunrise;
+        var sunset = data.sys.sunset;
+        
+        // Substituting class id with jQuery actions.
+        if (weatherIcon == "Clear Sky"){
+          $(".weatherIcon").attr('class', '.icon.sunny.sun.rays');
+        }
+
+
+        $(".city").html(city);
+        $(".country").html(country);
+        $(".description").html(description);
+        $(".temp").html(temp);
+        $(".pressure").html(pressure);
+        $(".humidity").html(humidity);
+        $(".visibility").html(visibility);
+        $(".wind").html(wind);
+        $(".sunrise").html(sunrise);
+        $(".sunset").html(sunset);
+      })
+
+    })
   }
 });
