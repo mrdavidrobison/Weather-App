aa6ca73648af289bd7969670c953f3bb2a338bbc
David Robison <mrdavidrobison@gmail.com>
2024-04-01 11:29:26 -0600

    convert to es6

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,29 @@
-var gulp = require('gulp');
-var jade = require('gulp-jade');
-var stylus = require('gulp-stylus');
-var browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+const jade = require('gulp-jade');
+const stylus = require('gulp-stylus');
+const browserSync = require('browser-sync').create();
 
+// Task to compile Jade files
 gulp.task('compile-jade', function() {
   return gulp.src('./src/**/*.jade')
     .pipe(jade({ pretty: true }))
     .pipe(gulp.dest('./dist'));
 });
 
+// Task to compile Stylus files
 gulp.task('compile-stylus', function () {
   return gulp.src('./src/css/**/*.stylus')
     .pipe(stylus())
     .pipe(gulp.dest('./dist/css'));
 });
 
+// Task to copy JavaScript files
 gulp.task('copy-js', function () {
   return gulp.src('./src/scripts/**/*.js')
     .pipe(gulp.dest('./dist/scripts'));
 });
 
+// Task to initialize BrowserSync
 gulp.task('sync', function() {
   browserSync.init({
     server: { baseDir: "./dist" },
@@ -28,6 +32,7 @@ gulp.task('sync', function() {
   });
 });
 
+// Default task to compile all assets and start BrowserSync
 gulp.task('default', gulp.series(['compile-jade', 'compile-stylus', 'copy-js', 'sync'], function(done) {
   // Watch for changes in source files and trigger corresponding tasks
   gulp.watch('./src/**/*.jade', gulp.series('compile-jade'));
